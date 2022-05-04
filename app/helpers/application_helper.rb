52baaaba6fcc244787c26af5812365293a5ffb3f
Victor Costa <victorcosta9743@gmail.com>
2022-05-04 15:12:53 -0300

    hotfix/estoque

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,14 @@
 module ApplicationHelper
-    require 'money'
     # Todos os helpers utilizados em toda aplicação
+    #gems utilizadas
+    require 'money'
+    
+    # formata a hora para dia/mês/ano
     def formatarData(data)
         data.strftime("%d/%m/%Y")
     end
 
+    #formata o valor monetário para BRL
     def formatarValor(valor)
         Money.from_cents(valor, "BRL").format
     end
